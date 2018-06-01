89740398367ac4e1c247738448a69b8aa5e380e6
MendezV <som.felipe66@gmail.com>
2018-06-01 11:46:55 -0500

    half-optimized Poisson MLE code for extracting vexations

--- Matlab_anaysis/MLE_Poiss/lineminPoiss.m
+++ Matlab_anaysis/MLE_Poiss/lineminPoiss.m
@@ -1,4 +1,4 @@
-function alpha=lineminPoiss(params,conjugdir,alpharoot, MaxPop, Nbins, Tframes, hist)
+function alpha=lineminPoiss(params,conjugdir,alpharoot, MaxPop, Nbins, Tframes, hist, N, Nfac, NexpAv)
 
 %%IN
 %%-params: Nbins values for the vexation at each bin ) corresponds to the position for current
@@ -20,19 +20,19 @@ function alpha=lineminPoiss(params,conjugdir,alpharoot, MaxPop, Nbins, Tframes, 
 
 tau=0.7; %%control parameter should be between 0 and 1
 c=0.005; %%control parameter should be between 0 and 1
-grad=logligradNaive(params ,  MaxPop, Nbins, Tframes, hist); %%gradient of the function to be minimized
+grad=logligradPoiss(params ,  MaxPop, Nbins, Tframes, hist, N, Nfac, NexpAv); %%gradient of the function to be minimized
 m=conjugdir'*grad/sqrt(conjugdir'*conjugdir); %%local slope of the function of alpha  along the search direction
 t=-c*m; %parameter that serves as lower bound in the Armijo?Goldstein condition 
 alpha=alpharoot; %%initializing alpha for the linesearch
 
-diff=logliNaive(params,  MaxPop, Nbins, Tframes, hist)-logliNaive(params+alpha*conjugdir,  MaxPop, Nbins, Tframes, hist); %%difference that will be minimized 
+diff=logliPoiss(params,  MaxPop, Nbins, Tframes, hist, N, Nfac, NexpAv)-logliPoiss(params+alpha*conjugdir,  MaxPop, Nbins, Tframes, hist, N, Nfac, NexpAv); %%difference that will be minimized 
 
 %%updating alpha until the Armijo-Goldstein condition is fulfilled, then we
 %%are satisfied with the value of alpha that will multiply the conjugate
 %%direction in the algorithm
 while diff<alpha*t
     alpha=alpha*tau;
-    diff=logliNaive(params,  MaxPop, Nbins, Tframes, hist)-logliNaive(params+alpha*conjugdir,  MaxPop, Nbins, Tframes, hist);
+    diff=logliPoiss(params,  MaxPop, Nbins, Tframes, hist, N, Nfac, NexpAv)-logliPoiss(params+alpha*conjugdir,  MaxPop, Nbins, Tframes, hist, N, Nfac, NexpAv);
 end
 
 end
--- Matlab_anaysis/MLE_Poiss/logliPoiss.m
+++ Matlab_anaysis/MLE_Poiss/logliPoiss.m
@@ -1,4 +1,4 @@
-function logp=logliPoiss(params ,  MaxPop, Nbins, Tframes, hist)
+function logp=logliPoiss(params ,  MaxPop, Nbins, Tframes, hist, N, Nfac, NexpAv)
 
 %%IN
 %%-params: Nbins values for the vexation at each bin ) corresponds to the position for current
@@ -16,12 +16,10 @@ function logp=logliPoiss(params ,  MaxPop, Nbins, Tframes, hist)
 
 %parameters that will be used in the calculation
 V=params; %%extracting vexation
-N=((1:(MaxPop+1))-1)'; %vector with possible occupation numbers in the system
 
 %%elements that will be needed for the calculation of the negative log of
 %%the probability
-z=sum(exp(-V*N')./gamma(ones(Nbins,1)*(N+1)'),2); %%vector that contains the partition function of each bin, size Nbinsx1
-NexpAv=hist*N; %experimental average number of flies at each bin
+z=sum(exp(-V*N')./Nfac,2); %%vector that contains the partition function of each bin, size Nbinsx1
 loggammaav=hist*log(gamma(N+1)); %%not necessary for the minimization, could do it but sums to
 %a huge number 
 logp=Tframes*sum(log(z)+V.*NexpAv+loggammaav)+sum(sum(gammaln(Tframes*hist+1)))-Nbins*gammaln(Tframes+1);
