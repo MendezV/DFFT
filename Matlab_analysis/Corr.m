ef7fce56f712917ecfe694319973050fc234e640
MendezV <som.felipe66@gmail.com>
2018-06-05 08:57:59 -0500

    changed quotes

--- Matlab_analysis/Corr.m
+++ Matlab_analysis/Corr.m
@@ -9,7 +9,7 @@ c=(mean( abs(ifft(abs(fft(counts)).^2))/size(counts,1) - ones(size(counts,1),1)*
 
 lit=[1:5]'; % first few points
 p=polyfit(lit,log(c(lit)),1); 
-fprintf("Correlation time (in frames)...\n");
+fprintf('Correlation time (in frames)...\n');
 tau=-1/p(1)
 
 
